a2e833d9b924a92b1974e7e64aefeb27c560a03a
Puneeth K <reachout.puneeth@gmail.com>
2024-12-24 10:29:38 +0530

    Added the line-through effect for the completed todos, and also removed the Done button for already completed todos

--- components/Todo.jsx
+++ components/Todo.jsx
@@ -4,12 +4,15 @@ const Todo = ({ id, title, description, mongoId, complete, deleteTodo, completeT
     return (
         <tr>
             <td className="border border-gray-300 px-4 py-2">{id + 1}</td>
-            <td className="border border-gray-300 px-4 py-2">{title}</td>
-            <td className="border border-gray-300 px-4 py-2">{description}</td>
+            <td className={`border border-gray-300 px-4 py-2 ${complete ? "line-through" : ""}`}>{title}</td>
+            <td className={`border border-gray-300 px-4 py-2 ${complete ? "line-through" : ""}`}>{description}</td>
             <td className="border border-gray-300 px-4 py-2">{complete ? "Completed" : "Not Completed"}</td>
             <td className="border border-gray-300 px-4 py-2 flex gap-1">
                 <button onClick={() => deleteTodo(mongoId)} className='py-2 px-4 bg-red-500 text-white'>Delete</button>
-                <button onClick={() => completeTodo(mongoId)} className='py-2 px-4 bg-green-500 text-white'>Done</button>
+                {
+                    complete ? " " : <button onClick={() => completeTodo(mongoId)} className='py-2 px-4 bg-green-500 text-white'>Done</button>
+
+                }
             </td>
         </tr>
     )
